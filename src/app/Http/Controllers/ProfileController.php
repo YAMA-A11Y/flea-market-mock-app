93835eae17b8f0d40f25f52fbb12e7cd80f1e1e2
yamamoto <yamamoto.m.58@outlook.jp>
2026-01-10 17:15:03 +0900

    Implement profile update feature with validation and persistence

--- src/app/Http/Controllers/ProfileController.php
+++ src/app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ProfileRequest;
 
 class ProfileController extends Controller
 {
@@ -13,17 +14,23 @@ class ProfileController extends Controller
         ]);
     }
 
-    public function update(Request $request)
+    public function update(ProfileRequest $request)
     {
         $user = $request->user();
 
-        $user->update([
-            'username' => $request->input('username'),
-            'postcode' => $request->input('postcode'),
-            'address' => $request->input('address'),
-            'building' => $request->input('building'),
-            'profile_completed' => true,
-        ]);
+        if($request->hasFile('profile_image')) {
+            $path = $request->file('profile_image')->store('profiles', 'public');
+            $user->profile_image = $path;
+        }
+        
+        $user->username = $request->input('username');
+        $user->postcode = $request->input('postcode');
+        $user->address = $request->input('address');
+        $user->building = $request->input('building');
+
+        $user->profile_completed = true;
+
+        $user->save();
 
         return redirect('/');
     }
--- src/resources/views/mypage/profile.blade.php
+++ src/resources/views/mypage/profile.blade.php
@@ -1,25 +1,95 @@
-<form method="POST" action="/mypage/profile">
-  @csrf
+@extends('layouts.app')
 
-  <div>
-    <label>ユーザー名</label>
-    <input type="text" name="username" value="{{ old('username', $user->username) }}">
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/profile.css') }}">
+@endsection
+
+@section('content')
+<div class="profile-form__content">
+  <div class="profile-form__heading">
+    <h2 class="page-title">プロフィール設定</h2>
   </div>
 
-  <div>
-    <label>郵便番号</label>
-    <input type="text" name="postcode" value="{{ old('postcode', $user->postcode) }}">
-  </div>
+  <form class="form" action="/mypage/profile" method="post" enctype="multipart/form-data" novalidate>
+    @csrf
 
-  <div>
-    <label>住所</label>
-    <input type="text" name="address" value="{{ old('address', $user->address) }}">
-  </div>
+    <div class="profile-form__image">
+      <div class="profile-form__image-preview"></div>
 
-  <div>
-    <label>建物名</label>
-    <input type="text" name="building" value="{{ old('building', $user->building) }}">
-  </div>
+      <label class="profile-form__image-button">
+        <input class="profile-form__image-input" type="file" name="profile_image" accept="image/*">
+        画像を選択する
+      </label>
 
-  <button type="submit">更新する</button>
-</form>
+      @error('profile_image')
+        <div class="form__error">{{ $message }}</div>
+      @enderror
+    </div>
+
+    <div class="form__group">
+      <div class="form__group-title">
+        <span class="form__label--item">ユーザー名</span>
+      </div>
+      <div class="form__group-content">
+        <div class="form__input--text">
+          <input type="text" name="username" value="{{ old('username', $user->username ?? '') }}" required autocomplete="username" />
+        </div>
+
+        @error('username')
+          <div class="form__error">{{ $message }}</div>
+        @enderror
+      </div>
+    </div>
+
+    <div class="form__group">
+      <div class="form__group-title">
+        <span class="form__label--item">郵便番号</span>
+      </div>
+      <div class="form__group-content">
+        <div class="form__input--text">
+          <input type="text" name="postcode" value="{{ old('postcode', $user->postcode ?? '') }}" required autocomplete="postal-code" />
+        </div>
+
+        @error('postcode')
+          <div class="form__error">{{ $message }}</div>
+        @enderror
+      </div>
+    </div>
+
+    <div class="form__group">
+      <div class="form__group-title">
+        <span class="form__label--item">住所</span>
+      </div>
+      <div class="form__group-content">
+        <div class="form__input--text">
+          <input type="text" name="address" value="{{ old('address', $user->address ?? '') }}" required autocomplete="street-address" />
+        </div>
+
+        @error('address')
+          <div class="form__error">{{ $message }}</div>
+        @enderror
+      </div>
+    </div>
+
+    <div class="form__group">
+      <div class="form__group-title">
+        <span class="form__label--item">建物名</span>
+      </div>
+      <div class="form__group-content">
+        <div class="form__input--text">
+          <input type="text" name="building" value="{{ old('building', $user->building ?? '') }}" />
+        </div>
+
+        @error('building')
+          <div class="form__error">{{ $message }}</div>
+        @enderror
+      </div>
+    </div>
+
+    <div class="form__button">
+      <button class="form__button-submit" type="submit">更新する</button>
+    </div>
+  </form>
+</div>
+@endsection
+
