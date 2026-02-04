38151bcebd3eee5a62e49d439d7f2fb601ebea88
yamamoto <yamamoto.m.58@outlook.jp>
2026-02-04 20:15:33 +0900

    Localize authentication error messages in Japanese

--- src/resources/lang/ja/validation.php
+++ src/resources/lang/ja/validation.php
@@ -3,8 +3,15 @@
 return [
     'required' => ':attributeを入力してください',
 
+    'min' => [
+        'string' => ':attributeは:min文字以上で入力してください',
+    ],
+
+    'confirmed' => ':attributeが一致しません',
+
     'attributes' => [
         'email' => 'メールアドレス',
         'password' => 'パスワード',
+        'password_confirmation' => '確認用パスワード',
     ],
 ];
