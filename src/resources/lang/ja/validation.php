f67e5883a8741a98cd782d99861baf044347982e
yamamoto <yamamoto.m.58@outlook.jp>
2026-02-28 14:22:42 +0900

    Add Japanese validation messages for max and regex rules

--- src/resources/lang/ja/validation.php
+++ src/resources/lang/ja/validation.php
@@ -7,11 +7,22 @@ return [
         'string' => ':attributeは:min文字以上で入力してください',
     ],
 
+    'max' => [
+        'string' => ':attributeは:max文字以内で入力してください',
+    ],
+
+    'regex' => ':attributeの形式が正しくありません',
+
     'confirmed' => ':attributeが一致しません',
 
     'attributes' => [
         'email' => 'メールアドレス',
         'password' => 'パスワード',
         'password_confirmation' => '確認用パスワード',
+        'profile_image' => 'プロフィール画像',
+        'username' => 'ユーザー名',
+        'postcode' => '郵便番号',
+        'address' => '住所',
+        'building' => '建物名',
     ],
 ];
